d340241f10146ad77a6e512985e9f3a398ebbca4
Niklas <33847121+Nivador@users.noreply.github.com>
2021-06-22 01:27:33 +0200

    Update netfilterqueue.pyx
    Increase BufferSize and MaxPacketSize

--- netfilterqueue.pyx
+++ netfilterqueue.pyx
@@ -14,7 +14,7 @@ COPY_PACKET = 2
 
 # Packet copying defaults
 DEF DEFAULT_MAX_QUEUELEN = 1024
-DEF MaxPacketSize = 0xFFFF
+DEF MaxPacketSize = 0xFFFF0
 DEF BufferSize = 4096
 DEF MetadataSize = 80
 DEF MaxCopySize = BufferSize - MetadataSize
